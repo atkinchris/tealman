586fa6b69770a899700185c280d2d6709c82d872
Mustafa Temel <mustafatemel90@gmail.com>
2019-09-06 14:13:20 +0100

    Add gulp task for processing SASS.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
 const del = require('del')
 const gulp = require('gulp')
+const gulpPostcss = require('gulp-postcss')
+const gulpRename = require('gulp-rename')
+const gulpSass = require('gulp-sass')
 const gulpZip = require('gulp-zip')
 
 const paths = {
   build: 'build',
   dist: 'dist',
   release: 'tealman.zip',
-  src: 'src',
+  src: 'src'
+}
+paths.styles = {
+  src: `${paths.src}/panel/styles/*.scss`,
+  output: 'bundle.css',
+  dest: `${paths.build}/panel`,
 }
 
 function clean () {
@@ -25,13 +35,21 @@ function copyRootAssets () {
   ]).pipe(gulp.dest(`${paths.build}`))
 }
 
+function styles () {
+  return gulp.src(paths.styles.src)
+    .pipe(gulpSass())
+    .pipe(gulpPostcss([autoprefixer(), cssnano()]))
+    .pipe(gulpRename(paths.styles.output))
+    .pipe(gulp.dest(paths.styles.dest))
+}
+
 function zip () {
   return gulp.src(`${paths.build}/*`)
     .pipe(gulpZip(`${paths.release}`))
     .pipe(gulp.dest(`${paths.dist}`))
 }
 
-const build = gulp.series(clean, copyRootAssets)
+const build = gulp.series(clean, copyRootAssets, styles)
 const release = gulp.series(build, zip)
 
 exports.clean = clean
