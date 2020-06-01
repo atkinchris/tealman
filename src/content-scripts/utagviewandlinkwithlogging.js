f76b66268412675856db77a19d290e67ee93266a
Mustafa Temel <mustafatemel90@gmail.com>
2020-06-01 01:25:46 +0100

    Add a content script which decorates utag.view and utag.link functions with logging.

---
+++ src/content-scripts/utagviewandlinkwithlogging.js
@@ -0,0 +1,37 @@
+const contentScript = document.createElement('script')
+contentScript.type = 'text/javascript'
+contentScript.async = true
+contentScript.id = 'tealman-utagviewandlinkwithlogging'
+contentScript.text = `
+  function decorateWithLogging (fn, logMessagePrefix = '') {
+    return function () {
+      const message = {...arguments[0]}
+      console.group('[Tealman]', logMessagePrefix)
+      console.log(message)
+      console.groupEnd()
+      const result = fn.apply(this, arguments)
+      return result
+    }
+  }
+
+  const delay = 125
+  const maxRetries = 32
+  let retries = 0
+
+  function wait () {
+    if (window.utag && typeof window.utag.view === 'function' && typeof window.utag.link === 'function') {
+      window.setTimeout(() => {
+        window.utag.view  = decorateWithLogging(window.utag.view, 'utag.view')
+        window.utag.link  = decorateWithLogging(window.utag.link, 'utag.link')
+      }, delay)
+    } else {
+      retries++
+      if (retries < maxRetries) {
+        window.setTimeout(wait, delay)
+      }
+    }
+  }
+
+  wait()
+`
+document.body.appendChild(contentScript)
